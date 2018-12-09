f9fd9e760c445fd4d9099557bf6860811cd1a994
StacyChebet <staceychebet23@gmail.com>
2018-12-09 15:44:17 +0300

    Add the setup test

---
+++ photos/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from .models import Photographer
+import datetime as dt 
+
+# Create your tests here.
+class PhotographerTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        self.stacy = Photographer(first_name = 'Stacy', last_name = 'Chebet', email = 'staceychebet23@gmail.com')
+
+    # #Testing instance
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.stacy,Photographer))
