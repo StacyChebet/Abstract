eabfaa51729b5f54ba24208651c303fba32c35f2
StacyChebet <staceychebet23@gmail.com>
2018-12-09 19:52:43 +0300

    Add the location foreign key to the Image model

--- photos/models.py
+++ photos/models.py
@@ -19,7 +19,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'pictures/')
     title = models.CharField(max_length = 60)
     description = models.CharField(max_length = 200)
-    # image_location = models.ForeignKey(Location)
+    image_location = models.ForeignKey(Location)
     # image_category = models.ForeignKey(Category)
 
 class Location(models.Model):
