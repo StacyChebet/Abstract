3831a041224034be456676046271ea1c84aeac6c
StacyChebet <staceychebet23@gmail.com>
2018-12-09 22:00:25 +0300

    Register the location model

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Photographer
+from .models import Photographer,Image,Location
 
 # Register your models here.
 admin.site.register(Photographer)
+admin.site.register(Image)
+admin.site.register(Location)
