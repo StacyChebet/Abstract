00819e09aee8ab013e9465232fcb3ae2c9f06a5f
StacyChebet <staceychebet23@gmail.com>
2018-12-09 19:13:50 +0300

    Add the save photographer function to the Photographer model

---
+++ photos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import datetime as dt
+
+# Create your models here.
+
+class Photographer(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
+
+    def __str__(self):
+        return self.first_name
+    
+    def save_photographer(self):
+        self.save()
+
+    
+
+    
