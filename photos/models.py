a9e41571578e948e4f1438603b7ab27e370a2fad
StacyChebet <staceychebet23@gmail.com>
2018-12-10 08:11:23 +0300

    Add the function to save the photographer object

--- photos/models.py
+++ photos/models.py
@@ -6,12 +6,11 @@ class Photographer(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
     
     def save_photographer(self):
-        def __str__(self):
-            return self.first_name
-    
-    def save_editor(self):
         self.save()
 
 
