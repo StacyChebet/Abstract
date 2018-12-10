430b087ce660513a3cdcd7b927f0c85ca86ffe71
StacyChebet <staceychebet23@gmail.com>
2018-12-10 09:59:57 +0300

    Register the photographer model

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Photographer,Image,Location
+from .models import Photographer
+
 
 # Register your models here.
 admin.site.register(Photographer)
-admin.site.register(Image)
-admin.site.register(Location)
