361df44ce91eb05d34bddfeb398ae5af015f6e9b
StacyChebet <staceychebet23@gmail.com>
2018-12-10 08:08:31 +0300

    Recreate the photographer model

--- photos/models.py
+++ photos/models.py
@@ -2,29 +2,19 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-
 class Photographer(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
-    phone_number = models.CharField(max_length = 10, blank = True)
-
-    def __str__(self):
-        return self.first_name
     
     def save_photographer(self):
+        def __str__(self):
+            return self.first_name
+    
+    def save_editor(self):
         self.save()
 
 
-class Location(models.Model):
-    location = models.CharField(max_length = 60)
-
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'pictures/')
-    title = models.CharField(max_length = 60)
-    description = models.CharField(max_length = 200)
-    image_location = models.ForeignKey(Location)
-    # image_category = models.ForeignKey(Category)
 
 
     
