ea9b5b10565c94d8060e0d3848665f1bef56ed7d
StacyChebet <staceychebet23@gmail.com>
2018-12-11 09:26:23 +0300

    Rwgister the Category model

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Photographer,Image
+from .models import Photographer,Image,Location,Category
 
 
 # Register your models here.
 admin.site.register(Photographer)
 admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
